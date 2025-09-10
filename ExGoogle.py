6c070157269b7df54d2d6355b58d700e24231324
Filipe de Almeida Quadro <quadrolipe@gmail.com>
2025-09-10 16:59:39 -0300

    ExGoogle.py

--- ExGoogle.py
+++ ExGoogle.py
@@ -1,2 +1,15 @@
+email = input("Digite seu email: ")
 
+emailBanco = "plexc@gmail.com"
+semhaBanco = "123@"
+
+if (email ==  emailBanco):
+    senha = input("Digite sua senha: ")
+    if (senha == semhaBanco):
+        print("Bem vindo PlexC!")
+    else:
+        print("Senha incorreta. Tente novamente.")
+elif (email != emailBanco):
+    print ("Não foi possível encontrar sua Conta do Google.")
+        
 
